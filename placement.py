0f51f8520b8d475a035ad648ab097996680d7b17
dific <dificalabc@gmail.com>
2022-12-07 18:25:12 +0500

    сортировка расчесткой

---
+++ placement.py
@@ -0,0 +1,18 @@
+massiv = [23, 91, 558, 55, 13]
+def comb(massiv):
+    step = int(len(massiv)/1.247)
+    swap = 1
+    while step > 1 or swap > 0:
+        swap = 0
+        i = 0
+        while i + step < len(massiv):
+            if massiv[i] > massiv[i+step]:
+                massiv[i], massiv[i+step] = massiv[i+step], massiv[i]
+                swap += 1
+            i = i + 1
+        if step > 1:
+            step = int(step / 1.247)
+            print(massiv)
+print(massiv)
+comb(massiv)
+print('Результат сортировки:', massiv)
